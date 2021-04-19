ef520562d857f5ca7cd6d4b16d3424725d8bd9cc
Victor Uriarte <uriarte0505@gmail.com>
2021-04-19 11:35:09 -0300

    Small code refactoring

--- eggplant-brownie/Controller/ViewController.swift
+++ eggplant-brownie/Controller/ViewController.swift
@@ -84,25 +84,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
-    
-
-    // MARK: - IBActions
-    @IBAction func adicionar(_ sender: Any) {
+    func recuperaRefeicaoDoFormulario() -> Refeicao? {
         guard let nomeDaRefeicao = nomeTextField?.text  else {
-            return
+            return nil
         }
 
         guard let felicidadeDaRefeicao = felicidadeTextField?.text, let felicidade = Int(felicidadeDaRefeicao) else {
-            return
+            return nil
         }
         
         let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade, itens: itensSelecionados)
-
-
-        print("Comi \(refeicao.nome) e fiquei com felicidade \(refeicao.felicidade)")
         
-        delegate?.add(refeicao)
-        navigationController?.popViewController(animated: true)
+        return refeicao
+    }
 
+    // MARK: - IBActions
+    @IBAction func adicionar(_ sender: Any) {
+        if let refeicao = recuperaRefeicaoDoFormulario() {
+            delegate?.add(refeicao)
+            navigationController?.popViewController(animated: true)
+        } else {
+            Alerta(controller: self).exibe(mensagem: "Erro ao ler dados do formuário")
+        }
     }
 }
